0a15b7ca4ae90a2eaa856c7e4929117d8c99d746
Ajay <ajayvasan.s@getnitro.co>
2025-12-12 19:37:09 +0530

    table population is pending nearly done

--- migrations/tenant/env.py
+++ migrations/tenant/env.py
@@ -63,6 +63,45 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    def migration_per_tenant(current_tenant):
+        with connectable.connect() as connection:
+            connection.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{current_tenant}_schema"'))
+            connection.execute(text(f'SET search_path TO "{current_tenant}_schema"'))
+            connection.commit()
+
+            context.configure(
+                connection=connection,
+                target_metadata=target_metadata,
+                version_table_schema=f"{current_tenant}_schema",
+                include_schemas=False,
+            )
+
+            with context.begin_transaction():
+                context.run_migrations()
+    def populate_meta(org_id:int , org_name:str):
+        with connectable.connect() as connection:
+            connection.execute(text(f'SET search_path TO public'))
+            connection.execute(
+                text("""
+                    INSERT INTO public.tenants 
+                    (org_id, org_name, schema_name, plan_type, is_active)
+                    VALUES 
+                    (:org_id, :org_name, :schema_name, :plan_type, :is_active)
+                """),
+                {
+                    "org_id": f"{org_id}",
+                    "org_name": f"{org_name}",
+                    "schema_name": f"{org_name}_schema",
+                    "plan_type": "basic",
+                    "is_active": True
+                }
+            )
+
+    """
+    some really fucking important shit
+    """
+
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -71,22 +110,16 @@ def run_migrations_online() -> None:
 
     current_tenant = context.get_x_argument(as_dictionary=True).get("tenant")
 
-    with connectable.connect() as connection:
+    if str(current_tenant) != "all_orgs_":
+        migration_per_tenant(current_tenant)
+    else:
+        for i in ["ajay_dev/01","ajay_dev_2/02","ajay_nitroo/03"]: #DB Public.Tenants(tenant meta data )
 
-        connection.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{current_tenant}"'))
-        connection.execute(text(f'SET search_path TO "{current_tenant}"'))
-        connection.commit()
+            migration_per_tenant(i.split("/")[0])
+            populate_meta(int(i.split("/")[1]),i.split("/")[0])
 
-        context.configure(
-            connection=connection, 
-            target_metadata=target_metadata,
-            version_table_schema=current_tenant,
-            include_schemas=False,
-        )
 
-        with context.begin_transaction():
-            context.run_migrations()
-
+    
 
 if context.is_offline_mode():
     run_migrations_offline()
